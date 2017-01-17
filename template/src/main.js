6198cfd8c42d7eb28b8246b18c1c87760608d637
Samuel Roth <s-roth@onu.edu>
2017-01-16 22:30:06 -0500

    Finishing touches

--- template/src/main.js
+++ template/src/main.js
@@ -5,6 +5,8 @@
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import VueRouter from 'vue-router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
+Vue.use(VueRouter){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 import Home from './components/Home'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
